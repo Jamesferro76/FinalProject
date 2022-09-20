0e3e50f4111e191662ec05bfa259f9759a01aa02
Jamesferro76 <jamesferromusic@gmail.com>
2022-09-20 16:51:44 -0600

    Stub JPA and Boot/REST

---
+++ Intersteller/src/main/java/com/skilldistillery/intersteller/services/AuthService.java
@@ -0,0 +1,11 @@
+package com.skilldistillery.intersteller.services;
+
+import com.skilldistillery.intersteller.entities.User;
+
+public interface AuthService {
+
+	public User register(User user);
+
+	public User getUserByUsername(String username);
+
+}
