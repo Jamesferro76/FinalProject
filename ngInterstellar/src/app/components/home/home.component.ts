a1c6efb45f3cba5393dafba187e96e5aa373a49e
zachkott <zachkott@gmail.com>
2022-09-22 10:08:51 -0700

    added code to home componet

--- ngInterstellar/src/app/components/home/home.component.ts
+++ ngInterstellar/src/app/components/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user';
+import { AuthService } from 'src/app/services/auth.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +10,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  newUser: User = new User();
 
-  constructor() { }
+  loginUser: User = new User();
+
+
+  constructor(private userServ: UserService, private auth: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
+
+  register(user: User): void {
+    console.log('Registering user:');
+    console.log(user);
+    this.userServ.register(user).subscribe({
+      next: (registeredUser) => {
+        this.auth.login(user.username, user.password).subscribe({
+          next: (loggedInUser: any) => {
+            this.router.navigateByUrl('/todo');
+          },
+          error: (problem: any) => {
+            console.error('RegisterComponent.register(): Error logging in user:');
+            console.error(problem);
+          }
+        });
+      },
+      error: (fail) => {
+        console.error('RegisterComponent.register(): Error registering account');
+        console.error(fail);
+      }
+    });
+  }
+
+
+
+
+  login(user: User) {
+    console.log(user);
+    this.auth.login(user.username, user.password).subscribe({
+      next: (loggedInUser: any) => {
+        this.router.navigateByUrl('todo');
+      },
+      error: (problem: any) => {
+        console.error('LoginComponent.login(): Error logging in user:');
+        console.error(problem);
+      },
+    });
+  }
+
+  logout(){
+    console.log("logout");
+    this.auth.logout();
+    this.router.navigateByUrl('home');
+
+    }
 
 }
