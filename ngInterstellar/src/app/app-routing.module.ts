0067a03a8690bd223de36a51d975b170307b9fa1
Daniel Schulenberg <dschumil@gmail.com>
2022-09-22 13:57:06 -0700

    Nav bar

--- ngInterstellar/src/app/app-routing.module.ts
+++ ngInterstellar/src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: HomeComponent },
   { path: 'profile', component: ProfileComponent },
+  { path: 'mixer', component: MixerComponent },
   { path: '**', component: NotFoundComponent },
 ];
 
